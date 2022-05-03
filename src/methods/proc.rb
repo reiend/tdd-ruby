d960f60b4fa5c045e3f450322f9f851cbc270c23
jake <loncerasjakerei@gmail.com>
2022-05-03 22:45:11 +0800

    Using do, end with procs

--- src/methods/proc.rb
+++ src/methods/proc.rb
@@ -2,8 +2,15 @@
 
 full_name = proc { |first_name:, last_name:| "#{first_name} #{last_name}" }
 
+character_name = proc do |first_name:, last_name:|
+  "#{first_name} #{last_name}"
+end
+
 # 1st option to call
+# call syntax
 p full_name.call first_name: 'Era', last_name: 'Are'
+p character_name.call first_name: 'Era', last_name: 'Are'
 
 # 2nd option to call
+# bracket syntax
 p full_name[first_name: 'Era', last_name: 'Are']
