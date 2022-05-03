7821c751db2ed45dabf7689fe9bf4f461e3966ed
jake <loncerasjakerei@gmail.com>
2022-05-03 22:27:05 +0800

    Procs call options

--- src/methods/proc.rb
+++ src/methods/proc.rb
@@ -2,4 +2,8 @@
 
 full_name = proc { |first_name:, last_name:| "#{first_name} #{last_name}" }
 
+# 1st option to call
 p full_name.call first_name: 'Era', last_name: 'Are'
+
+# 2nd option to call
+p full_name[first_name: 'Era', last_name: 'Are']
